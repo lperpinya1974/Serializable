aefc1f021ea4c5980ac722ea70a30523f3027a6d
lperpinya1974 <lperpinya1974@gmail.com>
2016-11-04 17:21:34 +0100

    Initial commit

---
+++ app/src/main/java/com/exemple/profedam/serializable/MainActivity.java
@@ -0,0 +1,75 @@
+package com.exemple.profedam.serializable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private MyObject myObject = new MyObject();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+     if (savedInstanceState!=null)
+     {
+         myObject = (MyObject) savedInstanceState.getSerializable("myObject");
+         rellenarTextView();
+     }
+        setContentView(R.layout.activity_main);
+
+
+
+        Button btnMostrar = (Button) findViewById(R.id.btn_mostrar);
+        Button btnCrear = (Button) findViewById (R.id.btn_crear);
+        btnMostrar.setOnClickListener(this);
+        btnCrear.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.btn_crear)
+        {
+            ArrayList<String> adreces = new ArrayList();
+            adreces.addAll(Arrays.asList((new String[] {"C/Major 10", "C/Menor 12"})));
+             myObject = new MyObject("name", (new Random()).nextInt(100), adreces);
+             Toast.makeText(this, "objeto creado", Toast.LENGTH_SHORT).show();
+        }
+        if (view.getId() == R.id.btn_mostrar)
+        {
+
+            rellenarTextView();
+
+
+        }
+
+
+    }
+
+    public void rellenarTextView() {
+        String frase = "Nombre: " + myObject.getName() + " Edad:" + myObject.getAge();
+        for (String contador:myObject.getAddress())
+        {
+            frase += " Direccion " + contador;
+        }
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(frase);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable("myObject", myObject);
+        super.onSaveInstanceState(outState);
+
+    }
+}
---
+++ app/src/main/java/com/exemple/profedam/serializable/MyObject.java
@@ -0,0 +1,48 @@
+package com.exemple.profedam.serializable;
+
+/**
+ * Created by Lluis_2 on 04/11/2016.
+ */
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MyObject implements Serializable {
+
+    private String name;
+    private int age;
+
+    public ArrayList<String> address;
+
+    public MyObject(String name, int age, ArrayList<String> address) {
+        super();
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public MyObject() {
+
+    }
+
+    public ArrayList<String> getAddress() {
+        if (!(address == null))
+            return address;
+        else
+            return new ArrayList<String>();
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
